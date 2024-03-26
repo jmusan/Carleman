e0670359adfabc8b6d36b2d7bcc1a488e80dccfd
Juan Carlos Muñoz Sánchez <jc.munoz@csic.es>
2024-03-26 10:17:11 +0100

    Delete Riccati directory

--- Riccati/functions/distLim_plot_Riccati.m
+++
@@ -1,7 +0,0 @@
-function [Y_plot,isPlot] = distLim_plot_Riccati(t,yteo,yexp,Lim)
-
-    dist = sqrt((yteo(:,1)-yexp(:,1)).^2)./abs(yteo(:,1));
-    isPlot = (dist<Lim);
-    Y_plot = [t(isPlot) yexp(isPlot,:) abs(yexp(isPlot,:)-yteo(isPlot,:))];
-
-end
